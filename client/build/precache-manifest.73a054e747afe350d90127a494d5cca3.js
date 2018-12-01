08bfbef10bdae599ec60e07efec7162c47875c36
Kurt Johnson <fazbat@gmail.com>
2018-12-01 09:01:31 -0800

    compressed home img

---
+++ client/build/precache-manifest.73a054e747afe350d90127a494d5cca3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "676fb49b9ecf0ad7d066ef6120cafb39",
+    "url": "/static/media/logo.676fb49b.png"
+  },
+  {
+    "revision": "a3d37276f4915035d3ded16bd922afe3",
+    "url": "/static/media/books4.a3d37276.jpg"
+  },
+  {
+    "revision": "cc5cb20057871dbfbc9f1b696e766230",
+    "url": "/static/media/avatar.cc5cb200.jpg"
+  },
+  {
+    "revision": "e5e0354514c70fd464177625d85ad530",
+    "url": "/static/media/ImageNotAvailable.e5e03545.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "144040a0ee5cb84f1828",
+    "url": "/static/js/main.144040a0.chunk.js"
+  },
+  {
+    "revision": "6be451f9f97a77ad230c",
+    "url": "/static/js/1.6be451f9.chunk.js"
+  },
+  {
+    "revision": "144040a0ee5cb84f1828",
+    "url": "/static/css/main.3fdf3f03.chunk.css"
+  },
+  {
+    "revision": "797013f1123ab9ab16665a5c95a8a3e7",
+    "url": "/index.html"
+  }
+];
